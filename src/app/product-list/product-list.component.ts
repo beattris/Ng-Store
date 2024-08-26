0b657c62ceeb6be1c5f354d6cb01d5ab913b0f08
beattris <beatriceebirim@gmail.com>
2024-08-26 17:02:41 +0100

    used property binding to display data

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -3,18 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
+  name = "John Doe"
   product = {
-    name: 'iPhone 15',
+    name: 'iPhone 15 pro max',
     price: 789,
     color: 'matte black',
     discount: 8.5,
-    inStock: 5
-  }
+    inStock: 5,
+    pImage:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNPmD3YNOZ_-bO-19Z7tEliZheuMJkoQJ12g&s',
+  };
 
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount) / 100; 
+  getDiscountedPrice() {
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
   }
 }
