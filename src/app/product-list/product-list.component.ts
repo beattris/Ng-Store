5923f0253aed4e09e86dd1d0cc0cd18ee7a73a7f
beattris <beatriceebirim@gmail.com>
2024-08-30 15:52:03 +0100

    used ngFor to modify our menu in the header

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,37 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-  // name = 'John Doe';
-  addToCart: number = 0;
-  product = {
-    name: 'iPhone 15 pro max',
-    price: 789,
-    color: 'matte black',
-    discount: 8.5,
-    inStock: 10,
-    pImage:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNPmD3YNOZ_-bO-19Z7tEliZheuMJkoQJ12g&s',
-  };
-
-  getDiscountedPrice() {
-    return (
-      this.product.price - (this.product.price * this.product.discount) / 100
-    );
-  }
-
-  // onNameChange(event: any) {
-  //   this.name = event.target.value;
-  // }
-
-  decrementCartValue() {
-    if (this.addToCart > 0) {
-      this.addToCart--;
-    }
-  }
-
-  incrementCartValue() {
-    if (this.addToCart < this.product.inStock) {
-      this.addToCart++;
-    }
-  }
+  listOfString: string[] = ['Tris', 'john', 'jane', 'Doe'];
 }
